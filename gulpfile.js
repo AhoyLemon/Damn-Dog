5b9d8fee2262a0dc28433f2f4332397118668042
Lemon <AhoyLemon@users.noreply.github.com>
2018-06-04 15:15:05 -0500

    LiveReload instead of BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,22 @@
 var gulp        = require('gulp'),
-    browserSync = require('browser-sync').create(),
+    //browserSync = require('browser-sync').create(),
+    livereload = require('gulp-livereload'),
     sass        = require('gulp-sass'),
     wait        = require('gulp-wait');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-      server: {
-        baseDir: './'
-      }
-    });
-
-    gulp.watch("sass/**/*.scss", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
-    gulp.watch("css/*.css").on('change', function() {
-      console.log('FUCK!')
-      browserSync.reload;
-    });
-});
-
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-  return gulp.src("./sass/damn.scss")
-    .pipe(wait(300))
+  gulp.src("./sass/damn.scss")
     .pipe(
       sass( { includePaths: ['./sass/partials'] })
       .on('error', sass.logError)
     )
     .pipe(gulp.dest("./css"))
-    .pipe(browserSync.stream())
-    .pipe(browserSync.reload);
+    .pipe(wait(600))
+    .pipe(livereload());
 });
 
-gulp.task('default', ['serve']);
+gulp.task('watch', function() {
+  livereload.listen({ basePath: './' });
+  gulp.watch("sass/**/*.scss", ['sass']);
+});
